d6400ec43999998653d1cd8314cfacca47fd02c1
sdclayton <96846005+sdclayton@users.noreply.github.com>
2023-03-23 18:49:49 -0500

    add: categories in seed file and additional (social) columns to blog

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+ Category.create(name: 'Leadership')
+ Category.create(name: 'Time Management')
+ Category.create(name: 'Goal-Setting')
+ Category.create(name: 'Communicaiton')
+ Category.create(name: 'Networking')
+ Category.create(name: 'Emotional Intelligence')
+ Category.create(name: 'Learning and Development')
+ Category.create(name: 'Career Development')
+ Category.create(name: 'Personal Development')
+
+
+
